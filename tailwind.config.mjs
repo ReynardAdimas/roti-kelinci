c5ee1e6d3fc01e62bd1c7daf18870f3a9a43f4a0
ReynardAdimas <dimastube22@gmail.com>
2024-12-29 18:17:27 +0700

    adding menu and order page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -6,6 +7,12 @@ export default {
 			colors:{
 				primary:'#FFECE9',
 				primaryTxt:'#D43C06',
+				list:'#E82561',
+				foot:'#FF66C4',
+				catalog:'#F4A6C8'
+			}, 
+			fontFamily:{
+				poppins: ['Poppins', 'sans-serif'],
 			},
 		},
 	},
